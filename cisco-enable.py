a3c405a4747373cb568de5457796e93f5c0b52f8
martinward1234 <martinward100@gmail.com>
2017-05-23 14:19:17 -0400

    updated carriage return new line syntax

--- cisco-enable.py
+++ cisco-enable.py
@@ -2,7 +2,7 @@
 
 # A script to ssh into a cisco device, set the terminal length
 # such that paging is turned off, then run commands.
-# the results go into 'resp', then are displayed. 
+# the results go into 'resp', then are displayed.
 # Tweak to your hearts content!
 
 import paramiko
@@ -22,25 +22,25 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 chan = ssh.invoke_shell()
 
 # first we enable!
-chan.send('enable\r')
+chan.send('enable\r\n')
 time.sleep(0.5)
 resp = chan.recv(1024)
 (print) (resp)
 
 # enablepassword!
-chan.send('python\r')
+chan.send('python\r\n')
 time.sleep(0.5)
 resp = chan.recv(1024)
 (print) (resp)
 
 # turn off paging
-chan.send('terminal length 0\r')
+chan.send('terminal length 0\r\n')
 time.sleep(0.5)
 resp = chan.recv(1024)
 (print) (resp)
 
 # Issue the Show Run command to prove script function
-chan.send('sh run\n')
+chan.send('sh run\r\n')
 time.sleep(7.5)
 resp = chan.recv(10240)
 (print) (resp.splitlines())
